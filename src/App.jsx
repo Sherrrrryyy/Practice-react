e94aa45db92b27c24b5f70d9c1ae6d1a255532a3
Sherrrrryyy <Shaheerkhan090909@gmail.com>
2024-11-02 16:12:08 +0500

    first

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+  // const [myCar, setMyCar] = useState({
+  //   car: "Land Cruiser",
+  //   color: "black",
+  //   year: 2022,
+  //   brand: "Toyota"
+
+  // })
+
+// useState Hook
+
+  // const changeCarColorAndBrand = () => {
+  //   setMyCar((prev)=>{
+  //     return{...prev , car : "Sonata" , color : "white" , brand : "hyundai"}
+  //   })
+
+  // }
+
+  // const [color, setColor] = useState("Black")
+  // const [year, setYear] = useState("2024")
+  // const [brand, setBrand] = useState("KIA")
+
+
+
+
+  // const incrementNum = () => {
+  //   setCount(count => count + 1)
+  //   setCount(count => count + 1)
+  //   setCount(count => count + 1)
+  //   setCount(count => count + 1)
+
+  // }
+
+// useState Hook
+
+
+
+// useEffect Hook
+
+
+
+
+
+  return (
+    <>
+
+{/* useState Hook */}
+
+      {/* <h1>My car is {myCar.car}</h1>
+      <p>It was {myCar.color} color and model of {myCar.year} year and his brand name is {myCar.brand}</p>
+
+      <button onClick={changeCarColorAndBrand}>Change color and brand name</button> */}
+      {/* <button onClick={incrementNum}>increase number</button> */}
+
+{/* useState Hook */}
+
+
+    </>
+  )
+}
+
+export default App
